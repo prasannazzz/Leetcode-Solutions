bdcbea37cce6c75326776fb1f0a16cbde28fe43b
Prasanna Pravin patil <proplayer14102004@gmail.com>
2024-11-23 09:00:15 +0530

    Time: 0 ms (100%), Space: 40.2 MB (69.46%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        } 
+        return true;
+           }
+}//identify 4 by trying out 3, 6 in brute forcing
